25b8341b820ae6b8a90332b2aacd3b07e2300e32
guobx <guobx@sports.cn>
2017-11-23 11:44:48 +0800

    微信投票
    微信投票

---
+++ Data/conf/db.php
@@ -0,0 +1,12 @@
+<?php
+return array (
+  'DB_TYPE' => 'mysql',
+  'DB_HOST' => 'localhost',
+  'DB_PORT' => '3306',
+  'DB_NAME' => 'xxx',   //数据库名
+  'DB_USER' => 'xxx',   //用户名
+  'DB_PWD' => 'xxx',   //密码
+  'DB_PREFIX' => 'tp_',
+  'agent_version' => 1,
+);
+?>
